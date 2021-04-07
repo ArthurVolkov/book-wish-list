3f7f159906a99b5a3410ce8c06bdf5c97f2ab093
izekgolland <izek.go@gmail.com>
2021-04-07 16:46:19 +0300

    booklist

--- src/cmps/BookPreview/BookPreview.jsx
+++ src/cmps/BookPreview/BookPreview.jsx
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import { Component } from 'react'
-=======
 import ReactStars from "react-rating-stars-component";
 import React from "react";
 import { render } from "react-dom";
->>>>>>> fc43ebe2c5d3086ddc482316dc8fecb0aae1d490
 
 export class BookPreview extends Component {
     state = {
@@ -16,7 +13,6 @@ export class BookPreview extends Component {
         else this.props.onRemoverFromWishList(this.props.book)
     }
 
-<<<<<<< HEAD
     render() {
         const { isOnWishList } = this.state
         return (
@@ -27,34 +23,17 @@ export class BookPreview extends Component {
                 </div>
                 <h4>{this.props.book.author}</h4>
                 <p className="descp">{this.props.book.description}</p>
-                <div>Rating: </div>
-            </div>
-        )
-    }
-}
-=======
-export function BookPreview({ book }) {
-    console.log('book:', book)
-
-    return (
-        <div className="flex flex-col justify-between align-center book-preview">
-            <div className="flex justify-between align-center">
-                <h3>{book.title}</h3>
-                <input type="checkbox" />
-            </div>
-            <h4>{book.author}</h4>
-            <p className="descp">{book.description}</p>
-            <div className="flex align-center rate-container">Rating: 
+                <div className="flex align-center rate-container">Rating: 
             <ReactStars
                     count={5}
                     size={24}
                     edit={false}
-                    value={+book.rating}
+                    value={this.props.book.rating}
                     activeColor="#ffd700"
                 />
-            </div>
-            <h5>Price: {book.price}</h5>
-        </div>
-    )
+                </div>
+                    <h5>Price: {+this.props.book.price}</h5>            
+                </div>
+        )
+    }
 }
->>>>>>> fc43ebe2c5d3086ddc482316dc8fecb0aae1d490
